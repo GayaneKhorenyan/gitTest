788f56d70ec7d07f2fa18603ee91e4f51f941a6f
GayaneKhorenyan <gayanekhorenyan@mail.ru>
2015-09-22 17:54:36 +0400

    commit_migrate

---
+++ application/migrations/001_add_cdusers.php
@@ -0,0 +1,45 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Migration_Add_cdusers extends CI_Migration
+{
+
+    public function up()
+    {
+        $this->dbforge->add_field(array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 5,
+                'auto_increment' => TRUE
+            ),
+            'first_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'last_name' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'email' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'password' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+            'image' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '255',
+            ),
+        ));
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->create_table('cdusers');
+    }
+
+    public function down()
+    {
+        $this->dbforge->drop_table('cdusers');
+    }
+}
